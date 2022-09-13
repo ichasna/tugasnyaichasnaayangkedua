b6437030ac90339c74db25fae4297419f7ca1147
syarifah <asyarifahnur@gmail.com>
2022-09-13 23:15:30 +0700

    komit pertama

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'nama': 'Syarifah Nur Amalia',
+    'NPM' : '2106751272',
+    'list_barang': data_barang_katalog,
+    }
+    return render(request, "katalog.html", context)
